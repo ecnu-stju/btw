4698c2c5ea5320cc4aeb749b1835414a2f2a3d59
Sophia <sophiazhang351@gmial.com>
2019-08-13 23:17:11 +0800

    云函数 publish_post 二次修改

--- cloud-functions/publish_post/index.js
+++ cloud-functions/publish_post/index.js
@@ -1,5 +1,3 @@
-publish 页面
-
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
@@ -16,7 +14,16 @@ exports.main = async (event, context) => {
         img_url: event.img_url,
         //this.data指这个页面的数据，所以用在本地页面，event 用在云函数
         content: event.content,
-
+        author_id: event.userInfo.openId,//特例
+        author_name: event.author_name,
+        author_avatar_url: event.author_avatar_url,
+        publish_time: Date.now(),
+        update_time: Date.now(),
+        image_url: event.image_url,//有图像的都要用
+        // update_time: event.update_time,
+        // 最近一次更新时间，发布或者评论触发更新,目前用服务器端时间
+        //以上基本是每个页面都需要的
+        watch_count: 1,//浏览数
         id: event.address.id,
         city_id: event.address.city_id,
         address: event.address.address,
