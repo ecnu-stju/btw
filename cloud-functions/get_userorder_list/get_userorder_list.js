9cb6f96a65e14a5dafea514358448a9aba591e7c
wybbbbb <wyb98013@163.com>
2020-02-05 17:56:11 +0800

    changshi

---
+++ cloud-functions/get_userorder_list/get_userorder_list.js
@@ -0,0 +1,22 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database({
+  env: "ecnu-project-50330f"
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return {
+    userorder: await db.collection('userorder_collection').field({
+      _id: true,
+      author_name: true,
+      content: true,
+      title: true,
+      update_time: true
+    }).orderBy('update_time', 'desc').get(),
+
+  }
+}
