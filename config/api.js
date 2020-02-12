0098e00f6b6ba2168697f252c47502e0ac9794bd
1 <957993857@qq.com>
2020-02-12 12:28:39 +0800

    fix: 搬运了地址页的js，注释了尚未完成的wx.requestPayment

--- config/api.js
+++ config/api.js
@@ -48,7 +48,7 @@ module.exports = {
   AddressList: ApiRootUrl + 'address/list',  //收货地址列表
   AddressDetail: ApiRootUrl + 'address/detail',  //收货地址详情
   AddressSave: ApiRootUrl + 'address/save',  //保存收货地址
-  AddressDelete: ApiRootUrl + 'address/delete',  //保存收货地址
+  AddressDelete: ApiRootUrl + 'address/delete',  //删除收货地址
 
   RegionList: ApiRootUrl + 'region/list',  //获取区域列表
 
--- pages/personal center/address/address.js
+++ pages/personal center/address/address.js
@@ -1,87 +1,63 @@
-// pages/authorize/authorize.js
+var util = require('../../../utils/util.js');
+var api = require('../../../config/api.js');
+var app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    addressList: [],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 页面初始化 options为页面跳转所带来的参数
+    this.getAddressList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
+    // 页面渲染完成
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+    // 页面显示
 
   },
+  getAddressList() {
+    let that = this;
+    util.request(api.AddressList).then(function (res) {
+      if (res.errno === 0) {
+        that.setData({
+          addressList: res.data
+        });
+      }
+    });
+  },
+  addressAddOrUpdate(event) {
+    console.log(event)
+    wx.navigateTo({
+      url: '/pages/ucenter/addressAdd/addressAdd?id=' + event.currentTarget.dataset.addressId
+    })
+  },
+  deleteAddress(event) {
+    console.log(event.target)
+    let that = this;
+    wx.showModal({
+      title: '',
+      content: '确定要删除地址？',
+      success: function (res) {
+        if (res.confirm) {
+          let addressId = event.target.dataset.addressId;
+          util.request(api.AddressDelete, { id: addressId }, 'POST').then(function (res) {
+            if (res.errno === 0) {
+              that.getAddressList();
+            }
+          });
+          console.log('用户点击确定')
+        }
+      }
+    })
+    return false;
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  },
   onHide: function () {
-
+    // 页面隐藏
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  bindGetUserInfo: function (e) {
-    console.log(e)
-    if (e.detail.userInfo) {//用户按了允许授权按钮
-      
-      console.log(e.detail.userInfo)
-      wx.setStorage({
-        key: 'userInfo',
-        data: e.detail.userInfo
-      })
-      var pages = getCurrentPages();             //  获取页面栈
-      var prevPage = pages[pages.length - 2];    // 上一个页面
-      prevPage.setData({
-        update: true
-      })
-      wx.navigateBack({
-        delta: 1
-      })
-
-    } else {//用户按了拒绝按钮
-    }
+    // 页面关闭
   }
 })
